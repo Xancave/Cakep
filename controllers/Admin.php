5d13430845a5ea0570cf9446f35e0d554f6268f0
Xancave <107187903+Xancave@users.noreply.github.com>
2023-11-16 13:52:11 +0700

    Update Admin.php
    bang ie kommet

--- controllers/Admin.php
+++ controllers/Admin.php
@@ -1,20 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Admin extends CI_Controller{
-
-  public function __construct(){
-		parent::__construct();
-    $this->load->model('M_admin');
-    $this->load->library('upload');
-	}
-
-  public function index(){
-    if($this->session->userdata('status') == 'login' && $this->session->userdata('role') == 1){
-      $data['avatar'] = $this->M_admin->get_data_gambar('tb_upload_gambar_user',$this->session->userdata('name'));
-      $data['stokBarangMasuk'] = $this->M_admin->sum('tb_barang_masuk','jumlah');
-      $data['stokBarangKeluar'] = $this->M_admin->sum('tb_barang_keluar','jumlah');      
-      $data['dataUser'] = $this->M_admin->numrows('user');
       $this->load->view('admin/index',$data);
     }else {
       $this->load->view('login/login');
