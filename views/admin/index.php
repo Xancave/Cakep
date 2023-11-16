89f59b4c3f761030fd40b42f49d32734f44ab0a8
Xancave <107187903+Xancave@users.noreply.github.com>
2023-11-16 14:28:08 +0700

    Add files via upload
    eee

--- views/admin/index.php
+++ views/admin/index.php
@@ -49,7 +49,7 @@
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><b>Y</b>t</span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>SC</b>GUDANG</span>
+        <span class="logo-lg"><b>SC</b>coook</span>
       </a>
       <!-- Header Navbar: style can be found in header.less -->
       <nav class="navbar navbar-static-top">
